bbad7aff5d068e2a3fcde028664e8762e5e073cf
Anja Skjaerbaek <45606242+anjskj@users.noreply.github.com>
2021-06-11 10:03:05 +0000

    created player-sprite

--- scenes/main.js
+++ scenes/main.js
@@ -12,6 +12,10 @@ addLevel([
   width: 30,
   height: 22,
   '^' : [sprite('space-invader')],
-  '!' : [sprite('wall')],  
-  '&' : [sprite('wall')]
+  '!' : [sprite('wall'), 'left-wall'],  
+  '&' : [sprite('wall'), 'right-wall'],
 })
+
+const player = add([
+  sprite('space-ship')
+  ])
